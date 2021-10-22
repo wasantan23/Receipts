c9c8219706f3c2479ddc2cbf33bf87be5a4f1b96
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2021-10-21 20:49:48 -0400

    Oct 21 2021 - It took me 25 minutes

--- Kiosk.java
+++ Kiosk.java
@@ -1,2 +1,57 @@
 
 //your main interface for the project, with the main method
+//Tanisha Wasan
+import java.util.Scanner; 
+
+public class Kiosk { // public class
+	
+	public static void main (String [] args) { //main method
+		
+		Scanner items = new Scanner (); //scanner
+		
+		public checkout () { //constructor
+			int pizza = 5; //variables
+			int chips = 1;
+			int coke = 2;
+			int sandwich 3;
+			int cereal = 6;
+			int price = 0; 
+			double tax = 0;
+			int totalItems = 0;
+			
+			System.out.println("Welcome to the kiosk! We are selling 5 items. Pizza for $5, chips for $1, coke for $2, sandwich for $3 and cereal for $6. Would you like to shop? Type 'Y' to shop and 'N' to exist.");
+			
+			String shop = Items.nextLine();
+			
+		} //closes the constructor
+			public void shopping() {
+				while (totalItems <= &* shop.equals("Y")||("y")) { //while loop 
+					System.out.println("Type out the first item you would like to buy.");
+					String firstItem = Items.nextLine();
+					System.out.println("Would you like to keep shopping?");
+					String shop = Items.nextLine();
+					System.out.println("What would you like to buy as your second item?");
+					String secondItem = Items.nextLine();
+					System.out.println("Would you like to keep shopping?");
+					String shop = Items.nextLine();
+					System.out.println("What would you like to buy as your third item?");
+					String thirdItem = Items.nextLine();
+					System.out.println("Would you like to keep shopping?");
+					String shop = Items.nextLine();
+					System.out.println("What would you like to buy as your fourth item?");
+					String fourthItem = Items.nextLine();
+					System.out.println("Would you like to keep shopping?");
+					String shop = Items.nextLine();
+					System.out.println("What would you like to buy as your fifth item?");
+					String fifthItem = Items.nextLine();
+					
+				}
+				
+				System.out.print("Thanks for shopping!");
+				
+			} //closes main method 
+		} //close class 
+		
+  //took me 25 minutes 
+  
+				
