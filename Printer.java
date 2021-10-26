0fc408774ee278824ac433e160d7d9eb4be0c139
wasantan23 <89757403+wasantan23@users.noreply.github.com>
2021-10-26 14:03:08 -0400

    Oct 26 2021 - Final Version (Added headers and aligned the decimal points)

--- Printer.java
+++ Printer.java
@@ -3,14 +3,15 @@
 public class Printer {
 
 
-public void printReciept(int firstItem, int secondItem, int thirdItem, int fourthItem, int fifthItem) { // printer class method
+public void printReceipt(int firstItem, int secondItem, int thirdItem, int fourthItem, int fifthItem) { // printer class method
 	double price = 0.0;
 
 	if (firstItem > 0) { //checking if the user inputed more than one pizza
 		double firstItemPrice = firstItem* 5;  //multiplying the number of pizzas by 5, the assigned price of pizza
 		price += firstItemPrice; // adding the price of the first item to the sub total
+		System.out.println("Tanisha's Shop Receipt");
 		System.out.println("****************************************");
-		System.out.println("QTY" + "\t ITEM \t \t " + "PRICE"); //up top header 
+		System.out.println("QTY" + "\t ITEM \t \t " + "      PRICE"); //up top header 
 		System.out.println(firstItem + "\t Pizza                 " + (String.format( "$%8.2f", firstItemPrice))); //tab is 4 spaces
 		
 	}
@@ -37,10 +38,11 @@ public void printReciept(int firstItem, int secondItem, int thirdItem, int fourt
 		double tax = price * 0.07;
 		
 		System.out.println("");
-		System.out.println("\t           Sub total \t " +                  (String.format( "$%8.2f", price)));
-		System.out.print("\t           Tax \t  \t " ); //calculating 7 percent tax by multiplying by 0.07
-		System.out.println(String.format( "$%8.2f", tax));
-		System.out.println("\t           Total \t " + (String.format( "$%8.2f", tax + price)));; //calculating the total
+		System.out.println("\t           Sub Total   " + (String.format("$%8.2f", price)));
+		System.out.print("\t           Tax \t       "); //calculating 7 percent tax by multiplying by 0.07
+		System.out.println(String.format("$%8.2f", tax));
+		System.out.println("\t           Total       " +  (String.format("$%8.2f", tax + price)));; //calculating the total
+		System.out.println("****************************************"); //  End of reciept
 } //closes the method
 } //closes the class
 
